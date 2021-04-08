f214aeef86d65ec507ca8d9abcf46051559d2e4e
saebom <boma91@gmail.com>
2021-04-08 23:22:38 +0900

    Add: 3d box 완성

--- 3d -boxes-background/index.js
+++ 3d -boxes-background/index.js
@@ -7,6 +7,7 @@ function createBoxes() {
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
             const box = document.createElement('div');  // <div> box 생성
+
             box.classList.add('box')  // .box css 속성 적용
             box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
             boxesContainer.appendChild(box)
