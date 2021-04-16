26a5f0e96acb01b93d169df6c4fec6f3c143e3d9
saebom <boma91@gmail.com>
2021-04-16 22:45:04 +0900

    Add: html생성

--- bouncing-ball/index.js
+++ bouncing-ball/index.js
@@ -7,7 +7,7 @@ document.addEventListener('click', function (e) {
     console.log(e.clientY)
     ball.style.left = (e.clientX - 25) + 'px';
     ball.style.top = (e.clientY - 25) + 'px';
-}, false);
+});
 
 // e.clientX 에서 25를 빼는 이유는, 공의 너비와 높이(각 50px)의 절반을 빼주는 것이다
 // e.clientX와 e.clientY는 top/left기준의 마우스가 닿는 절대값을 나타내므로
